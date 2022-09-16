2304b6247a5832c114b9039c0bb16c07a947ff99
Takaki Miyajima <takaki.mz@gmail.com>
2022-09-16 14:53:58 -0700

    added MUI and document.tsx etc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,29 @@
+import { useEffect } from 'react'
+import Head from 'next/head'
 import '@/styles/globals.css'
 import '@/styles/destyle.css'
 import type { AppProps } from 'next/app'
+import CssBaseline from '@material-ui/core/CssBaseline'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles && jssStyles.parentElement) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, [])
+
+  return (
+    <>
+      <CssBaseline />
+      <Head>
+        <title>A takaki miyajima</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
---
+++ src/pages/_document.tsx
@@ -0,0 +1,44 @@
+import { ServerStyleSheets } from '@material-ui/core/styles'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+import React from 'react'
+
+export default class MyDocument extends Document {
+  render(): JSX.Element {
+    return (
+      <Html lang="ja-JP">
+        <Head>
+          {/* PWA primary color */}
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+MyDocument.getInitialProps = async (ctx) => {
+  const sheets = new ServerStyleSheets()
+  const originalRenderPage = ctx.renderPage
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    })
+
+  const initialProps = await Document.getInitialProps(ctx)
+
+  return {
+    ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  }
+}
