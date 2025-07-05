5083a461852fce22f46cf2a33ac03298fb6fff17
Takaki Miyajima <takaki.mz@gmail.com>
2025-07-05 17:33:34 +0900

    feat: add renewal files

--- src/hooks/gio/index.ts
+++
@@ -1,3 +0,0 @@
-export { useGioContext } from './use-gio-context'
-export { GioContextProvider } from './GioContext'
-export type { Context } from './GioContext'
--- src/libs/client.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from 'microcms-js-sdk'
-
-const client = createClient({
-  serviceDomain: 'a-takaki-miyajima',
-  apiKey: process.env.NEXT_PUBLIC_API_KEY || '',
-})
-
-export { client }
-export * from 'microcms-js-sdk'
